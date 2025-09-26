12ae60d1cdeb16b3c924b82affd64106603f2d93
weverett10 <willeverett0508@gmail.com>
2025-09-26 10:38:27 -0400

    Add files via upload

--- Everett_Will_Whats_In_A_Name.py
+++ Everett_Will_Whats_In_A_Name.py
@@ -5,96 +5,79 @@ Date: 9/30/25
 description: practicing functions for CS2: getting different aspects of a name
 Bugs: names dont work if you have more than 2 middle names ormore than 2 lasts. names dont work if you dont input all 3 names
 challenges: sorted aray, menu, title
-sources: Mr Campbell and Ms. Marciano and Maddox auerelius Monterisi
+sources: Mr Campbell and Ms. Marciano
  '''
+def remove_title(name):
+    titles = ['Dr.', 'Ms.', 'Mrs.', 'Jr.', 'Sr.', 'III', 'IV', 'PhD', 'MD', 'Professor', 'sir', 'lord', 'lady', 'king']
+    names = name.split(' ')
 
+    for title in titles:
+        if title in names:
+            names.remove(title)
+    return ' '.join(names)
 def reverse(name):
-    reverse_name = lower(name[::-1])
-    return(reverse_name)
+    new = remove_title(name)
+    return lower(new[::-1])
 def count_vowels(name):
+    new = remove_title(name)
     count = 0
     vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    for character in name:
+
+    for character in new:
         if character in vowels:
             count += 1
-    return (count)
+    return count
 def count_consanants(name):
+    new = remove_title(name)
     count_con = 0
     vowels = ['a', 'e', 'i', 'o', 'u']
-    for charactors in name:
+
+    for charactors in new:
         if charactors not in vowels:
             count_con += 1
-    return (count_con)
+    return count_con
 def first_name(name):
-    titles = ['Dr.', 'Ms.', 'Mrs.', 'Jr.', 'Sr.', 'III', 'IV', 'PhD', 'MD', 'Professor', 'sir', 'lord', 'lady', 'king']
-    parts = name.split(" ")
-
-
-    if parts [0] in titles:
-        new_name = parts.remove(parts[0])
-        name_1 = parts[0]
-        
-
-    else:
-        name_1 = parts[0]
-        
-    
-
-    return(name_1) 
+    new = remove_title(name)
+    names = new.split(' ')
+    return names[0]
 def last_name(name, which):
-    titles = ['Dr.', 'Ms.', 'Mrs.', 'Jr.', 'Sr.', 'III', 'IV', 'PhD', 'MD', 'Professor', 'sir', 'lord', 'lady', 'king']
-    parts = name.split(" ")
-    if which == 'middle' and parts[0] in titles:
-        name_3 = parts[2] + ' ' + parts[3]
-    elif which == 'middle' and parts[0] not in titles:
-        name_3 = parts[3]
-        upper(name_3[0])
-    elif which == 'last' and parts[0] in titles:
-        name_3 = parts[3] + " " + parts[4]
-    elif which == 'last' and parts[0] not in titles:
-        name_3 = parts[2] + " " + parts[3]
+    new = remove_title(name)
+    names = new.split(' ')
+    if which == 'last':
+        return names[-1]+' '+names[-2]
     else:
-        name_3 = parts[2]
-
-    return(name_3) 
-def middle_name(name):  
-    titles = ['Dr.', 'Ms.', 'Mrs.', 'Jr.', 'Sr.', 'III', 'IV', 'PhD', 'MD', 'Professor', 'sir', 'lord', 'lady', 'king']
-    parts = name.split(" ")
-
-    if parts [0] in titles:
-        name_2 = parts[2]
+        return names[-1]
+def middle_name(name,which ):  
+    new = remove_title(name)
+    names = new.split(' ')
+    if which == 'last':
+        return ' '.join(names[1:-2])
     else:
-        name_2 = parts[1]  
-
-
-    return(name_2)
+        return ' '.join(names[1:-1])
 def hyphen(name):
-    hyphen = '-'
-    if hyphen in name:
-        return True
-    else:
-        return False
+    return hyphen in name
 def upper(name):
-    name_out = ' '
+    name_out = ''
+
     for letter in name:
         if ord(letter) < 122 and ord(letter) > 91:
             num = ord(letter)
-            num = num - 32
+            num -= 32
             letter = chr(num)
-            name_out = name_out + letter
+            name_out += letter
         else:
-            name_out = name_out + letter
-    return(name_out)
+            name_out += letter
+    return name_out
 def lower(name):
-    name_out = ' '
+    name_out = ''
     for letter in name:
         if ord(letter) > 64 and ord(letter) < 91:
             num = ord(letter)
-            num = num + 32
+            num += 32
             letter = chr(num)
-            name_out = name_out + letter
+            name_out += letter
         else:
-            name_out = name_out + letter
+            name_out += letter
     return(name_out)
 def scramble(name):
     shuffled = ''
@@ -105,40 +88,28 @@ def scramble(name):
         letter = random.choice(letters)
         shuffled += letter
         letters.remove(letter)
-    return(shuffled)
+    return shuffled 
 def palindrome(name):
-    reversed = reverse(name)
-    name = name
-    if lower(name) == reversed:
-        return True
-    else:
-        return False
+    return name == reverse(name)
 def sorted_array(name):
-    order = ' '
     real_name = name.replace(' ', '')
     chars = list(real_name)
     sorted_chars = sorted(chars)
-    order = order = sorted_chars
-    return(order)
+    return ' '.join(sorted_chars)
 def check_for_initials(name): 
-    final_initials = ' '
-    first = first_name(name)
-    second = middle_name(name)
-    third = last_name(name)
+    initials = ''
+    new = remove_title(name)
+    names = new.split(' ')
 
-    first_initial = first[0]
-    second_initial = second[0]
-    third_initial = third[0]
-
-    final_initials = (first_initial + second_initial + third_initial)
-    return(upper(final_initials))
+    for n in names:
+        initials += n[0]
+    return initials
 def check_for_title(name):
     titles = ['Dr.', 'Ms.', 'Mrs.', 'Jr.', 'Sr.', 'III', 'IV', 'PhD', 'MD', 'Professor', 'sir', 'lord', 'lady', 'king']
-    for charactors in titles:
-        if charactors in name:
+    for title in titles:
+        if title in name:
             return True
-        else:
-            return False
+    return False
 def secret_challenge(name):
     pass
 
@@ -174,11 +145,11 @@ def main():
                 elif selection == '3':
                     print(count_consanants(name))
                 elif selection == '4':
-                    print(first_name(name))
+                    print(first_name(name, which))
                 elif selection == '5':
                     print(last_name(name, which))
                 elif selection == '6':
-                    print(middle_name(name))
+                    print(middle_name(name,which))
                 elif selection == '7':
                     print(hyphen(name))
                 elif selection == '8':
