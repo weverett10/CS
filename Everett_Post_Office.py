f75f3db1f1070205b4a4d9b649b810a78e54a211
weverett10 <willeverett0508@gmail.com>
2025-10-20 20:35:51 -0400

    Add files via upload

--- Everett_Post_Office.py
+++ Everett_Post_Office.py
@@ -1,89 +1,70 @@
-#condense!!
-#change input to a list like 4,4,4,0001,00111
-
 def interpretinfo(info):
-    infoo = info.split(',')
-    card_length = infoo[0]
-    card_height = infoo[1]
-    card_thickness = infoo[2]
-    coming_from = infoo[3]
-    going_to = infoo[4]
+    info = info.split(',')
+    card_length = float(info[0])
+    card_height = float(info[1])
+    card_thickness = float(info[2])
+    coming_from = int(info[3])
+    going_to = int(info[4])
     return card_length, card_height, card_thickness, coming_from, going_to
-
-print(interpretinfo('4,4,4,00000908,0000000090'))
-
-def card_type(card_length, card_height, card_thickness):
+def get_card_type(card_length, card_height, card_thickness):
     pre_perim = card_length + card_height + card_thickness
     perim = pre_perim*2
 
-    card_type = ''
     if card_length >= 3.5 and card_length <= 4.25 and card_height >=3.5 and card_height<= 6 and card_thickness >= 0.007 and card_thickness <= 0.016:
-        card_type += 'post card'
-        return 'PCset'
-    elif card_length >= 4.25 and card_length <= 6 and card_height >= 6 and card_height<= 11.5 and card_thickness >= 0.007 and card_thickness <= 0.015:
-        card_type += 'large post card'
-        return 'LPCset'
-    elif card_length >= 3.5 and card_length <= 6.125 and card_height >= 5 and card_height<= 11.5 and card_thickness >= 0.016 and card_thickness <= 0.25:
-        card_type += 'envelope'
-        card_set = 'Envset'
-    elif card_length >= 6.125 and card_length <= 24 and card_height >= 11 and card_height<= 18 and card_thickness >= 0.25 and card_thickness <= 0.5:
-        card_type += 'large envelope'
-        card_set = 'LEnvset'
-    elif card_length >= 25 and card_height >= 11 and card_thickness >= 18 and perim <= 84:
-        card_type += 'package'
-        card_set = 'Packset'
-    elif card_length >= 25 and card_height >= 11 and card_thickness >= 18 and perim >= 85 and perim <= 130:
-        card_type += 'large package'
-        card_set = 'LPackset'
+        return 'post card'
+    elif card_length > 4.25 and card_length < 6 and card_height > 6 and card_height < 11.5 and card_thickness >= 0.007 and card_thickness <= 0.015:
+        return  'large post card'
+    elif card_length >= 3.5 and card_length <= 6.125 and card_height >= 5 and card_height<= 11.5 and card_thickness > 0.016 and card_thickness < 0.25:
+        return  'envelope'
+    elif card_length > 6.125 and card_length < 24 and card_height >= 11 and card_height<= 18 and card_thickness >= 0.25 and card_thickness <= 0.5:
+        return  'large envelope'
+    elif card_length > 24 and card_height > 11 and card_thickness > 18 and perim <= 84:
+        return  'package'
+    elif card_length > 24 and card_height > 11 and card_thickness > 18 and perim > 84 and perim <= 130:
+        return  'large package'
     else:
-        card_type += 'unmailable'
-    return card_type, card_set
-def zones(zipcode):
-    if 1 <= zipcode <= 6999:
+        return 'unmailable'
+def get_zone(zipcode):
+
+    if zipcode >= 1 and zipcode <= 6999:
         return 1
+    elif zipcode  >= 7000 and zipcode <= 19999:
+        return 2
+    elif zipcode  >= 20000 and zipcode <= 3599:
+        return 3
+    elif zipcode  >= 36000 and zipcode <= 62999:
+        return 4
+    elif zipcode  >= 63000 and zipcode <= 84999:
+        return 5
+    elif zipcode  >= 8500 and zipcode <= 99999:
+        return 6
+def get_distance(coming_from, going_to):
+    comingfromzone = get_zone(coming_from)
+    goingtozone = get_zone(going_to)
 
-
-
-
-def find_price(coming_from, going_to, card_length, card_height, card_thickness):
-    post_card = 0.03
-    PCset = 0.20
-    large_post_card = 0.03
-    LPCset = 0.37
-    envelope = 0.04
-    Envset = 0.60
-    large_envelop = 0.05
-    LEnvset = 0.60
-    package = 0.25
-    Packset = 2.95
-    large_package = 0.35
-    LPackset = 3.95
-
-    zones = zones(coming_from,going_to)
-    card_type = card_type(card_length, card_height, card_thickness)
-    set = set(card_type)
-
-    cost = 0
-    cost += set
-    cost += zones*card_type
-
-    return cost
-
+    if comingfromzone == -1 or goingtozone == -1:
+        return 'unmailable'
+    return abs(comingfromzone - goingtozone)
+def calculate_cost(card_type, distance):
+    if card_type == 'unmailable' or distance == 'unmailable':
+        return 'Unmailable'
+    if card_type == 'post card':
+        return .2 + 0.03*distance
+    elif card_type == 'large post card':
+        return .37 + 0.03*distance
+    elif card_type == 'enevelope':
+        return .37 + .04*distance
+    elif card_type == 'large envelope':
+        return  .6 + 0.05*distance
+    elif card_type == 'package':
+        return  2.95 + 0.25*distance
+    elif card_type == 'large package':
+        return  3.95 + 0.35*distance
 def main():
-    info = 'what is the length, height, and thickness f your car, and what zone your mailing from, and to' 
-
-    card_length = input('what is the length of your card')
-    card_height = input('what is the heigh of your card')
-    card_thickness = input('what is the thickness of your card')
-    coming_from = input('what zone are you mailing from')
-    to = input('what zone are you mailing to')
-
-    mailable = card_type(card_length, card_height, card_thickness)
-
-    if mailable == 'unmailable':
-        print('your productis unmailable')
-    else:
-        print(f'the final price to mail your is', find_price(coming_from, to, card_length, card_height, card_thickness))
-
+    info = input('what is the length, height, and thickness of your item, and what zone your mailing from, and to: ')
+    card_length, card_height, card_thickness, coming_from, going_to = interpretinfo(info)
+    card_type = get_card_type(card_length, card_height, card_thickness)
+    distance = get_distance(coming_from, going_to)
+    print(calculate_cost(card_type, distance))
 
 main()
