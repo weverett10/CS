21b814c2589c9564c0a8074a8f3122bad401b1c3
weverett10 <willeverett0508@gmail.com>
2026-04-08 14:35:24 -0400

    Add files via upload

--- Everett_Will_TicTacToe.py
+++ Everett_Will_TicTacToe.py
@@ -1,8 +1,20 @@
-
-
+'''
+Author: Will Everett
+Date: 4/7/2026
+description: game of tic tac toe
+Bugs: doesn't tell you who won
+sources: Mr Campbell and Ms. Marciano and Ms. Sharma
+ '''
 
 
 def playermove(playerup,board):
+    '''
+    description: decides which players turn it is
+
+    args: player up and board
+
+    returns: which players move it is and where it goes
+    '''
     while True:
         while True:
             column = input('which column do you want to put it in? (1 2 or 3): ')
@@ -19,14 +31,22 @@ def playermove(playerup,board):
 
 
 
-        if board[int(row)-1][int(column)-1] == '':
+        if board[int(row)-1][int(column)-1] == ' ':
             board[int(row)-1][int(column)-1] = playerup
             break
         else:
-            print("Can't do that!!!")
+            print("Can't do that!!!")#fix this
             break
 
 def win(board):
+    '''
+    description: figures out who wins the game
+
+    args: board
+
+    returns: true, telling if the game has been won
+    
+    '''
 
     if board[0][0] == 'x' and board [0][1] == 'x' and board [0][2] == 'x':
         return True
@@ -60,28 +80,38 @@ def win(board):
 
 
     elif board[0][0] == 'o' and board[1][0] == 'o' and board [2][0] == 'o':
-        return True
+        return ('o, you won')
     elif board[0][1] == 'o' and board[1][1] == 'o' and board [2][1] == 'o':
-        return True
+        return ('o, you won')
     elif board[0][2] == 'o' and board[1][2] == 'o' and board [2][2] == 'o':
-        return True
+        return ('o, you won')
 
         
     elif board[0][0] == 'o' and board[1][1] == 'o' and board [2][2] == 'o':
-        return True
+        return ('o, you won')
     elif board[0][2] == 'o' and board[1][1] == 'o' and board [2][0] == 'o':
         return True
 
 def tiegame(board):  
+    '''
+    description: checks if the game was a tie
+
+    args: board
+
+    returns: true, determines if the gaem is a tie
+    
+    '''
+
     if board[0][0] != ' ' and board[0][1] != ' ' and board[0][2] != ' ' and board[1][0] != ' ' and board[1][1] != ' ' and board[1][2] != ' ' and board[2][0] != ' ' and board[2][1] != ' ' and board[2][2] != ' ':
         return True
         
 
 def main():
+
     board =   [  
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]] 
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]] 
     
     playerup = 'x'
 
@@ -105,27 +135,27 @@ def main():
         else:
             playerup = 'x'
 
-        if win(board) is True:
+        if win(board):
             print('\n\n congrats you won')
             q = input('do you want to play again? (y/n): ')
             if q.lower() == 'y':   
                 board =   [  
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]]     
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]]     
                 continue
             else:
 
                 break
 
-        elif tiegame is True:
+        elif tiegame():
             print("\n\nit's a tie")
             q = input('do you want to play again? (y/n): ')            
             if q.lower() == 'y':        
                 board =   [  
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]] 
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]] 
         
                 continue
             else:
